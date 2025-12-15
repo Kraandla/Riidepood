0d1988adfb287546f772e4648e3591f5fd98101b
Kraandla <kevinrandla22@gmail.com>
2025-12-15 18:48:36 +0200

    Working(?) front end

---
+++ frontend-site/riidepood-frontend/src/components/ProductsTable.vue
@@ -0,0 +1,22 @@
+<script> 
+    export default {
+        name: "ProductsTable",
+        props: {
+            items:Array
+        }
+    }
+
+</script>
+
+<template> 
+    <table class="table table-striped">
+        <tr>
+            <th>Product ID</th>
+            <th>Product name</th>
+        </tr>
+        <tr v-for="item in items" :key="item.FilmID">
+            <td>{{item.ProductID}}</td>
+            <td>{{item.Name}}</td>
+        </tr>
+    </table>
+</template>
--- frontend-site/riidepood-frontend/src/router/index.ts
+++ frontend-site/riidepood-frontend/src/router/index.ts
@@ -16,6 +16,11 @@ const routes: Array<RouteRecordRaw> = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
+  {
+    path: '/products',
+    name: 'products',
+    component: () => import('../views/ProductsView.vue')
+  }
 ];
 
 const router = createRouter({
