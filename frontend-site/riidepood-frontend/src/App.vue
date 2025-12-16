59f88dd293631fd6a0fa5680bf1d3236028e4946
Kraandla <kevinrandla22@gmail.com>
2025-12-16 12:35:42 +0200

    Changed about to products in navbar

--- frontend-site/riidepood-frontend/src/App.vue
+++ frontend-site/riidepood-frontend/src/App.vue
@@ -5,8 +5,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-    <router-link to="/products">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/products">Products</router-link>
   </nav>
   <router-view />
 </template>
